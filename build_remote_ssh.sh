f335b206699c4fb6bc9a255a0e9848881f139478
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-03-14 09:01:45 +0100

    Ansible common_defaults role moved to separate directory TravisCI changed

--- build_remote_ssh.sh
+++ build_remote_ssh.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -eux
 
-REMOTE_IP="192.168.121.82"
-REMOTE_USER="vagrant"
+REMOTE_IP="192.168.121.70"
+REMOTE_USER="root"
 
-#Build ALL images (this takes several hours)
-#ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER ansible/build_remote_ssh.yml
+# Build ALL images (this takes several hours)
+ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER ansible/build_remote_ssh.yml
 
-#Build single image - windows_10:virtualbox
+# Build single image - windows_10:virtualbox
 #ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER --extra-vars 'run_script="./build.sh windows_10:virtualbox"' ansible/build_remote_ssh.yml
 
-#Build multiple images - windows_10 (virtualbox, libvirt) and ubuntu-server-16.04 (libvirt)
-ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER --extra-vars 'run_script="./build.sh windows_10:virtualbox windows_10:libvirt ubuntu-server-16.04:libvirt"' ansible/build_remote_ssh.yml
+# Build multiple images - windows_10 (virtualbox, libvirt) and ubuntu-server-16.04 (libvirt)
+#ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER --extra-vars 'run_script="./build.sh windows_10:virtualbox windows_10:libvirt ubuntu-server-16.04:libvirt"' ansible/build_remote_ssh.yml
+
