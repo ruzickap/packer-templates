73226acebe75495fed2f2369818a07899ded559b
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-03-15 17:22:52 +0100

    my_windows-10 added, build.sh simplified

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ export PACKER_CACHE_DIR="$TMPDIR"
 export VIRTIO_WIN_ISO_URL="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win.iso"
 export VIRTIO_WIN_ISO=$(basename $VIRTIO_WIN_ISO_URL)
 export LOG_DIR="$TMPDIR"
+export HEADLESS=${HEADLESS:-true}
+export PACKER_BINARY="packerio"
 
 readonly PROGNAME=$(basename $0)
 readonly ARGS="$@"
@@ -20,9 +22,10 @@ This script can build the various libvirt and virtualbox images.
 You should have Packer, Ansible, libvirt and VirtualBox installed.
 
 List of all supported builds:
- * windows_10             (libvirt, virtualbox)
- * windows_2016           (libvirt, virtualbox)
- * windows_2012_r2        (libvirt, virtualbox)
+ * my_windows-10          (libvirt, virtualbox)
+ * windows-10             (libvirt, virtualbox)
+ * windows-2016           (libvirt, virtualbox)
+ * windows-2012_r2        (libvirt, virtualbox)
  * ubuntu-desktop-17.10   (libvirt)
  * ubuntu-server-16.04    (libvirt)
  * ubuntu-server-14.04    (libvirt)
@@ -33,14 +36,14 @@ List of all supported builds:
 Examples:
 
 Build Windows 10 Enterprise Evaluation, Windows Server 2016 Evaluation and Windows Server 2012 Evaluation for Virtualbox:
-  $PROGNAME windows_10:virtualbox windows_2016:virtualbox windows_2012_r2:virtualbox
+  $PROGNAME my_windows-10:virtualbox windows-10:virtualbox windows-2016:virtualbox windows-2012_r2:virtualbox
 
 
 Build Windows 10 Enterprise Evaluation, Windows Server 2016 Evaluation and Windows Server 2012 Evaluation for libvirt:
-  $PROGNAME windows_10:libvirt windows_2016:libvirt windows_2012_r2:libvirt
+  $PROGNAME my_windows-10:libvirt windows-10:libvirt windows-2016:libvirt windows-2012_r2:libvirt
 
 
-Build Ubuntu Desktop 17.10, Ubuntu Server 16.04, 14.04, My Ubuntu Server 16.04, 14.04 and My Centos 7 for libvirt:
+Build Ubuntu Desktop 17.10, Ubuntu Server 16.04, 14.04, My Ubuntu Server 16.04, 14.04 and My CentOS 7 for libvirt:
   $PROGNAME ubuntu-desktop-17.10:libvirt ubuntu-server-16.04:libvirt ubuntu-server-14.04:libvirt my_ubuntu-server-16.04:libvirt my_ubuntu-server-14.04:libvirt my_centos-7:libvirt
 EOF
 }
@@ -48,7 +51,7 @@ EOF
 cmdline() {
   BUILDS=$@
 
-  if [ -z $BUILDS ]; then
+  if [ -z "$BUILDS" ]; then
     usage
     exit 0;
   fi
@@ -56,23 +59,70 @@ cmdline() {
   for BUILD in $BUILDS; do
     export PACKER_VAGRANT_PROVIDER="${BUILD##*:}"
     export MYBUILD="${BUILD%:*}"
+    export MY_NAME=`echo $MYBUILD | awk -F '-' '{ print $1 }'`
+
     echo "*** $MYBUILD - $PACKER_VAGRANT_PROVIDER"
 
     case $MYBUILD in
       *centos*)
-        export MY_NAME=`echo $MYBUILD | awk -F '-' '{ print $1 }'`
         export CENTOS_VERSION=`echo $MYBUILD | awk -F '-' '{ print $2 }'`
-        eval centos
+        export CENTOS_TAG=`curl -s ftp://ftp.cvut.cz/centos/$CENTOS_VERSION/isos/x86_64/sha1sum.txt | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1`
+        export CENTOS_ARCH="x86_64"
+        export CENTOS_TYPE="NetInstall"
+        export NAME="${MY_NAME}-${CENTOS_VERSION}-${CENTOS_ARCH}"
+
+        sudo dnf upgrade -y ansible
+
+        packer_build ${MY_NAME}-${CENTOS_VERSION}.json
       ;;
       *ubuntu*)
-        export MY_NAME=`echo $MYBUILD | awk -F '-' '{ print $1 }'`
         export UBUNTU_TYPE=`echo $MYBUILD | awk -F '-' '{ print $2 }'`
         export UBUNTU_MAJOR_VERSION=`echo $MYBUILD | awk -F '-' '{ print $3 }'`
-        eval ubuntu
+        export UBUNTU_ARCH="amd64"
+        export UBUNTU_VERSION=`curl -s http://releases.ubuntu.com/${UBUNTU_MAJOR_VERSION}/SHA1SUMS | sed -n "s/.*ubuntu-\([^-]*\)-${UBUNTU_TYPE}-${UBUNTU_ARCH}.iso/\1/p" | head -1`
+        export NAME="${MY_NAME}-${UBUNTU_VERSION::5}-${UBUNTU_TYPE}-${UBUNTU_ARCH}"
+
+        sudo dnf upgrade -y ansible
+
+        packer_build ${MY_NAME}-${UBUNTU_TYPE}.json
       ;;
-      windows*)
-        eval ${MYBUILD}
+      *windows-10*)
+        export WINDOWS_VERSION="10"
+        export WINDOWS_ARCH="x64"
+        export WINDOWS_EDITION="enterprise"
+        export NAME="${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
+
+        # Do no use latest ansible 2.4.2 for now (Gathering Facts is not working properly on Windows + WinRM)
+        sudo dnf install -y ansible-2.4.0.0-1.fc27
+
+        packer_build ${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-eval.json
       ;;
+      *windows-2016*)
+        export WINDOWS_VERSION="2016"
+        export WINDOWS_ARCH="x64"
+        export WINDOWS_TYPE="server"
+        export WINDOWS_EDITION="standard"
+        export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
+
+        # Do no use latest ansible 2.4.2 for now (Gathering Facts is not working properly on Windows + WinRM)
+        sudo dnf install -y ansible-2.4.0.0-1.fc27
+
+        packer_build ${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.json
+      ;;
+      *windows-2012_r2*)
+        export WINDOWS_VERSION="2012"
+        export WINDOWS_RELEASE="r2"
+        export WINDOWS_ARCH="x64"
+        export WINDOWS_TYPE="server"
+        export WINDOWS_EDITION="standard"
+        export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-${WINDOWS_RELEASE}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
+
+        # Do no use latest ansible 2.4.2 for now (Gathering Facts is not working properly on Windows + WinRM)
+        sudo dnf install -y ansible-2.4.0.0-1.fc27
+
+        packer_build ${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.json
+      ;;
+
     esac
   done
 }
@@ -97,69 +147,7 @@ packer_build() {
   esac
 
   echo -e "\n\n*** $NAME [$PACKER_FILE] [$PACKER_BUILDER_TYPE]\n"
-  packerio build -only="$PACKER_BUILDER_TYPE" -color=false -var 'headless=true' $PACKER_FILE | tee "${LOG_DIR}/${NAME}-${PACKER_BUILDER_TYPE}-packer.log"
-}
-
-# Expected enviroments: UBUNTU_TYPE="desktop" UBUNTU_MAJOR_VERSION="17.10" MY_NAME="ubuntu"
-ubuntu() {
-  export UBUNTU_ARCH="amd64"
-  export UBUNTU_VERSION=`curl -s http://releases.ubuntu.com/${UBUNTU_MAJOR_VERSION}/SHA1SUMS | sed -n "s/.*ubuntu-\([^-]*\)-${UBUNTU_TYPE}-${UBUNTU_ARCH}.iso/\1/p" | head -1`
-  export NAME="${MY_NAME}-${UBUNTU_VERSION::5}-${UBUNTU_TYPE}-${UBUNTU_ARCH}"
-
-  sudo dnf upgrade -y ansible
-
-  packer_build ${MY_NAME}-${UBUNTU_TYPE}.json
-}
-
-# Expected enviroments: CENTOS_VERSION="7" MY_NAME="ubuntu"
-centos() {
-  export CENTOS_TAG=`curl -s ftp://ftp.cvut.cz/centos/$CENTOS_VERSION/isos/x86_64/sha1sum.txt | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1`
-  export CENTOS_ARCH="x86_64"
-  export CENTOS_TYPE="NetInstall"
-  export NAME="${MY_NAME}-${CENTOS_VERSION}-${CENTOS_ARCH}"
-
-  sudo dnf upgrade -y ansible
-
-  packer_build my_centos-${CENTOS_VERSION}.json
-}
-
-windows_2012_r2() {
-  export WINDOWS_VERSION="2012"
-  export WINDOWS_RELEASE="r2"
-  export WINDOWS_ARCH="x64"
-  export WINDOWS_TYPE="server"
-  export WINDOWS_EDITION="standard"
-  export NAME="windows-${WINDOWS_TYPE}-${WINDOWS_VERSION}-${WINDOWS_RELEASE}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-
-  # Do no use latest ansible 2.4.2 for now (Gathering Facts is not working properly on Windows + WinRM)
-  sudo dnf install -y ansible-2.4.0.0-1.fc27
-
-  packer_build windows-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.json
-}
-
-windows_2016() {
-  export WINDOWS_VERSION="2016"
-  export WINDOWS_ARCH="x64"
-  export WINDOWS_TYPE="server"
-  export WINDOWS_EDITION="standard"
-  export NAME="windows-${WINDOWS_TYPE}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-
-  # Do no use latest ansible 2.4.2 for now (Gathering Facts is not working properly on Windows + WinRM)
-  sudo dnf install -y ansible-2.4.0.0-1.fc27
-
-  packer_build windows-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.json
-}
-
-windows_10() {
-  export WINDOWS_VERSION="10"
-  export WINDOWS_ARCH="x64"
-  export WINDOWS_EDITION="enterprise"
-  export NAME="windows-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-
-  # Do no use latest ansible 2.4.2 for now (Gathering Facts is not working properly on Windows + WinRM)
-  sudo dnf install -y ansible-2.4.0.0-1.fc27
-
-  packer_build windows-${WINDOWS_VERSION}-${WINDOWS_EDITION}-eval.json
+  $PACKER_BINARY build -only="$PACKER_BUILDER_TYPE" -color=false -var "headless=$HEADLESS" $PACKER_FILE 2>&1 | tee "${LOG_DIR}/${NAME}-${PACKER_BUILDER_TYPE}-packer.log"
 }
 
 
--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND="noninteractive"
 
 # Update the box
 # Use apt instead of apt-get to upgrade kernel as well. apt-get doesn't upgrade kernel by default...
-apt update > /dev/null
-apt -y upgrade > /dev/null
+apt-get update -qq > /dev/null
+apt-get upgrade -qq -y > /dev/null
