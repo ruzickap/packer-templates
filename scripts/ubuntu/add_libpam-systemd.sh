17a6605a145fe56941e50f4906513e9cabf26af7
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-04-07 12:43:30 +0200

    Fix https://serverfault.com/questions/706475/ssh-sessions-hang-on-shutdown-reboot for Ubuntu Xenial

---
+++ scripts/ubuntu/add_libpam-systemd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+# This script should fix problems with hanged ssh session when the Ubuntu server is rebooted
+# The same problem appear when using "vagrant halt"
+# I saw this issue in minimal installation for Xenial
+# Description: https://serverfault.com/questions/706475/ssh-sessions-hang-on-shutdown-reboot
+
+export DEBIAN_FRONTEND="noninteractive"
+apt-get install -qq -y libpam-systemd
