d847eb2d3d66bcab23a3eecc5796b7ce05b9a13b
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-05-03 19:14:37 +0200

    preseed.cfg taken from upstream, /etc/default/spice-vdagentd removed

--- scripts/ubuntu/spice-vdagent.sh
+++ scripts/ubuntu/spice-vdagent.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -eux
 
-
 if grep -q QEMU /proc/scsi/scsi; then
   echo "*** Installing spice-vdagent"
 
   export DEBIAN_FRONTEND="noninteractive"
   apt-get install -y -qq --no-install-recommends spice-vdagent
-
-  # https://bugs.launchpad.net/ubuntu/+source/spice-vdagent/+bug/1633609
-  echo 'SPICE_VDAGENTD_EXTRA_ARGS="-X"' > /etc/default/spice-vdagentd
 fi
