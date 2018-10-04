344378df891161b462128060262b55dd16426e79
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-10-04 04:13:37 +0200

    vagrant_init_destroy_boxes_docker box file location fix

--- vagrant_init_destroy_boxes_docker.sh
+++ vagrant_init_destroy_boxes_docker.sh
@@ -5,8 +5,11 @@ LOGDIR=${LOGDIR:-/var/tmp}
 BOXES_LIST=${*:-`find . -maxdepth 1 \( -name "*ubuntu*.box" -o -name "*centos*.box" -o -name "*windows*.box" \) -printf "%f\n" | sort | tr "\n" " "`}
 
 vagrant_box() {
-  local VAGRANT_BOX_FILE=$1
+  local VAGRANT_BOX_FILE_FULL_PATH=$1
+  local VAGRANT_BOX_FILE_BASE_DIR=$(dirname $VAGRANT_BOX_FILE_FULL_PATH)
+  local VAGRANT_BOX_FILE_BASE_NAME=$(basename $VAGRANT_BOX_FILE_FULL_PATH)
 
+  docker pull peru/vagrant_libvirt_virtualbox
   docker run --rm -t -u $(id -u):$(id -g) --privileged --net=host \
   -e HOME=/home/docker \
   -e LOGDIR=/home/docker/vagrant_logdir \
@@ -15,7 +18,7 @@ vagrant_box() {
   -v $VAGRANT_INIT_DESTROY_BOXES_SCRIPT_PATH:/home/docker/vagrant_script \
   -v $VAGRANT_BOX_FILE_BASE_DIR:/home/docker/vagrant \
   -v $LOGDIR:/home/docker/vagrant_logdir \
-  peru/vagrant_libvirt_virtualbox /home/docker/vagrant_script/vagrant_init_destroy_boxes.sh $VAGRANT_BOX_FILE
+  peru/vagrant_libvirt_virtualbox /home/docker/vagrant_script/vagrant_init_destroy_boxes.sh /home/docker/vagrant/$VAGRANT_BOX_FILE_BASE_NAME
 }
 
 
@@ -26,14 +29,13 @@ vagrant_box() {
 main() {
   for VAGRANT_BOX_FILE in $BOXES_LIST; do
     VAGRANT_BOX_FILE_FULL_PATH=$(readlink -f $VAGRANT_BOX_FILE)
-    VAGRANT_BOX_FILE_BASE_DIR=$(dirname $VAGRANT_BOX_FILE_FULL_PATH)
 
-    if [ ! -f $VAGRANT_BOX_FILE ]; then
-      echo -e "\n*** ERROR: Box file \"$VAGRANT_BOX_FILE\" does not exist!\n"
+    if [ ! -f $VAGRANT_BOX_FILE_FULL_PATH ]; then
+      echo -e "\n*** ERROR: Box file \"$VAGRANT_BOX_FILE_FULL_PATH\" does not exist!\n"
       exit 1
     fi
 
-    vagrant_box $VAGRANT_BOX_FILE
+    vagrant_box $VAGRANT_BOX_FILE_FULL_PATH
   done
 }
 
