60bb7521878de0d03d95a65e970141557469dc45
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-02-22 07:09:19 +0100

    Rewrite of scripts/playbook for preparing remote machines for builds

--- build_remote_ssh_ubuntu.sh
+++ build_remote_ssh_ubuntu.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -eux
 
-REMOTE_IP="192.168.121.161"
-REMOTE_USER="vagrant"
+REMOTE_IP="172.16.246.243,172.16.242.252,172.16.241.47"
+REMOTE_USER="ubuntu"
 
-# Build ALL images (this takes several hours)
+# Prepare remote machine for build
 ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER ansible/build_remote_ssh_ubuntu.yml
-
-# Build single image - windows-10-enterprise-x64-eval-virtualbox
-#ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER --extra-vars 'run_script="./build.sh windows-10-enterprise-x64-eval-virtualbox"' ansible/build_remote_ssh.yml
-
-# Build multiple images - windows_10 (virtualbox, libvirt) and ubuntu-server-18.04 (libvirt)
-#ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER --extra-vars 'run_script="./build.sh windows-10-enterprise-x64-eval-virtualbox windows-10-enterprise-x64-eval-libvirt ubuntu-18.04-server-amd64-libvirt"' ansible/build_remote_ssh.yml
