71ed87b4dc3eaf8b2f2c258bfb04003524611e4a
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-03-03 06:33:38 +0100

    Ansible now use python3, Using Virtualbox from upstream, GitLab registration added

--- build_remote_ssh_ubuntu.sh
+++ build_remote_ssh_ubuntu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
-REMOTE_IP="172.16.246.17,172.16.241.102"
+REMOTE_IP="172.16.241.102,172.16.246.17"
 REMOTE_USER="ubuntu"
 GITLAB_REGISTRATION_TOKEN=${GITLAB_REGISTRATION_TOKEN:-}
 
 # Prepare remote machine for build
 if [ -n "$GITLAB_REGISTRATION_TOKEN" ]; then
-  ansible-playbook -i "$REMOTE_IP," -e GITLAB_REGISTRATION_TOKEN="$GITLAB_REGISTRATION_TOKEN" --user $REMOTE_USER ansible/build_remote_ssh_ubuntu.yml
+  ansible-playbook -i "$REMOTE_IP," -e GITLAB_REGISTRATION_TOKEN="$GITLAB_REGISTRATION_TOKEN" -e ansible_python_interpreter=python3 --user $REMOTE_USER ansible/build_remote_ssh_ubuntu.yml
 else
-  ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER ansible/build_remote_ssh_ubuntu.yml
+  ansible-playbook -i "$REMOTE_IP," -e ansible_python_interpreter=python3 --user $REMOTE_USER ansible/build_remote_ssh_ubuntu.yml
 fi
