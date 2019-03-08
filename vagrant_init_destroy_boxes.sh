7e72da6b0ce8094e4d3f16c438f8f859bed65b51
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-03-08 14:00:24 +0100

    Change default for LOGDIR variable to /var/tmp/

--- vagrant_init_destroy_boxes.sh
+++ vagrant_init_destroy_boxes.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 BOXES_LIST=${*:-`find . -maxdepth 1 \( -name "*ubuntu*.box" -o -name "*centos*.box" -o -name "*windows*.box" \) -printf "%f\n" | sort | tr "\n" " "`}
 TMPDIR=${TMPDIR:-/var/tmp/vagrant_init_destroy_boxes}
-LOGDIR=${LOGDIR:-$TMPDIR}
+LOGDIR=${LOGDIR:-/var/tmp/}
 export VAGRANT_IGNORE_WINRM_PLUGIN=true
 
 
@@ -97,7 +97,6 @@ main() {
       rmdir $VAGRANT_CWD
     done
 
-    rmdir $TMPDIR
   fi
 }
 
--- vagrant_init_destroy_boxes_docker.sh
+++ vagrant_init_destroy_boxes_docker.sh
@@ -4,6 +4,7 @@ set -o pipefail
 
 VAGRANT_INIT_DESTROY_BOXES_SCRIPT_PATH=$PWD
 TMPDIR=${TMPDIR:-/var/tmp/vagrant_init_destroy_boxes}
+LOGDIR=${LOGDIR:-/var/tmp/}
 
 BOXES_LIST=${*:-`find . -maxdepth 1 \( -name "*ubuntu*.box" -o -name "*centos*.box" -o -name "*windows*.box" \) -printf "%f\n" | sort | tr "\n" " "`}
 # User docker / podman executable
@@ -23,11 +24,13 @@ vagrant_box() {
   $DOCKER_COMMAND run --rm -t -u $(id -u):$(id -g) --privileged --network host --name "vagrant_libvirt_virtualbox_${VAGRANT_BOX_FILE_BASE_NAME}" \
   -e HOME=/home/docker \
   -e TMPDIR=$TMPDIR \
+  -e LOGDIR=$LOGDIR \
   -v /dev/vboxdrv:/dev/vboxdrv \
   -v /var/run/libvirt/libvirt-sock:/var/run/libvirt/libvirt-sock \
   -v $VAGRANT_INIT_DESTROY_BOXES_SCRIPT_PATH:/home/docker/vagrant_script \
   -v $VAGRANT_BOX_FILE_BASE_DIR:/home/docker/vagrant \
   -v $TMPDIR:$TMPDIR \
+  -v $LOGDIR:$LOGDIR \
   peru/vagrant_libvirt_virtualbox /home/docker/vagrant_script/vagrant_init_destroy_boxes.sh /home/docker/vagrant/$VAGRANT_BOX_FILE_BASE_NAME
 }
 
@@ -50,7 +53,6 @@ main() {
     vagrant_box $VAGRANT_BOX_FILE_FULL_PATH
   done
 
-  rmdir $TMPDIR
 }
 
 main
