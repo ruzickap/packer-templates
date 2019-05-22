cdab1eca7885fe7d7e214c97b4ae7869626f21f6
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-05-22 21:42:43 +0200

    ansible_python_interpreter variable removed - not needed with Ansible 2.8

--- tools/build_remote_ssh_ubuntu.sh
+++ tools/build_remote_ssh_ubuntu.sh
@@ -6,7 +6,7 @@ GITLAB_REGISTRATION_TOKEN=${GITLAB_REGISTRATION_TOKEN:-}
 
 # Prepare remote machine for build
 if [ -n "$GITLAB_REGISTRATION_TOKEN" ]; then
-  ansible-playbook -i "$REMOTE_IP," -e GITLAB_REGISTRATION_TOKEN="$GITLAB_REGISTRATION_TOKEN" -e ansible_python_interpreter=python3 --user $REMOTE_USER ../ansible/build_remote_ssh_ubuntu.yml
+  ansible-playbook -i "$REMOTE_IP," -e GITLAB_REGISTRATION_TOKEN="$GITLAB_REGISTRATION_TOKEN" --user $REMOTE_USER ../ansible/build_remote_ssh_ubuntu.yml
 else
-  ansible-playbook -i "$REMOTE_IP," -e ansible_python_interpreter=python3 --user $REMOTE_USER ../ansible/build_remote_ssh_ubuntu.yml
+  ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER ../ansible/build_remote_ssh_ubuntu.yml
 fi
