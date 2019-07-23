9130903187272608ac7f0da86e3e9c9789e77a29
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-07-23 11:15:04 +0200

    Fix #22 - vagrant user password expiration

--- scripts/win-common/sysprep.bat
+++ scripts/win-common/sysprep.bat
@@ -1,1 +1,5 @@
-C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:A:\unattend.xml
+mkdir %WINDIR%\Setup\Scripts
+echo wmic useraccount where "name='vagrant'" set passwordexpires=false > %WINDIR%\Setup\Scripts\SetupComplete.cmd
+echo wmic useraccount where "name='Administrator'" set passwordexpires=false >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
+
+%WINDIR%\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:A:\unattend.xml
