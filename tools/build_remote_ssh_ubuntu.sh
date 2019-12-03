1b4dacd6f42dc2aa31505722e0bb20acd281bb0e
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-12-03 17:07:45 +0100

    build_remote_ssh_ubuntu.yml rewritten to use GH Actions and self-hosted runner

--- tools/build_remote_ssh_ubuntu.sh
+++ tools/build_remote_ssh_ubuntu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -eux
 
-REMOTE_IP="172.16.245.253,172.16.246.17"
-REMOTE_USER="ubuntu"
-GITLAB_REGISTRATION_TOKEN=${GITLAB_REGISTRATION_TOKEN:-}
+REMOTE_IP="company-nb"
+REMOTE_USER="pruzicka"
+TOKEN=${TOKEN:-AxxxxxxxxxxxxxxxxxxxxxxxxxxxO}
 
 # Prepare remote machine for build
-if [ -n "$GITLAB_REGISTRATION_TOKEN" ]; then
-  ansible-playbook -i "$REMOTE_IP," -e GITLAB_REGISTRATION_TOKEN="$GITLAB_REGISTRATION_TOKEN" --user $REMOTE_USER ../ansible/build_remote_ssh_ubuntu.yml
+if [ -n "$TOKEN" ]; then
+  ansible-playbook -i "$REMOTE_IP," -e TOKEN="$TOKEN" --user $REMOTE_USER ../ansible/build_remote_ssh_ubuntu.yml
 else
-  ansible-playbook -i "$REMOTE_IP," --user $REMOTE_USER ../ansible/build_remote_ssh_ubuntu.yml
+  echo "Missing TOKEN"
+  exit 1
 fi
