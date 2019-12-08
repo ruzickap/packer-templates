cf8532f69963fef451f13cb2d8e83ffb59e090b2
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-12-08 14:54:50 +0100

    build_all.sh changed to pass shellcheck

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -6,10 +6,10 @@ LOGFILE="/tmp/build_all.log"
 
 
 (
-
+  cd ..
   for PACKER_VAGRANT_PROVIDER in libvirt virtualbox; do
-    for BUILD in ubuntu-{19.04,18.10}-desktop-amd64 ubuntu-{18.04,16.04,14.04}-server-amd64 my_ubuntu-{18.04,16.04}-server-amd64 my_centos-7-x86_64 my_windows-10-enterprise-x64-eval windows-{server-{2019,2016,2012_r2}-standard,10-enterprise}-x64-eval; do
-      echo "**** `date`"
+    for BUILD in ubuntu-{19.04,19.10}-desktop-amd64 ubuntu-{18.04,16.04,14.04}-server-amd64 my_ubuntu-{18.04,16.04}-server-amd64 my_centos-7-x86_64 my_windows-10-enterprise-x64-eval windows-{server-{2019,2016,2012_r2}-standard,10-enterprise}-x64-eval; do
+      echo "**** $(date)"
       ./build.sh ${BUILD}-${PACKER_VAGRANT_PROVIDER}
     done
   done
