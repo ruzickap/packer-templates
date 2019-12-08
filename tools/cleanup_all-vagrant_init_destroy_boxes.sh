650521940ed6ca3e72db5058d942e8e88b7707b0
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-12-08 14:56:32 +0100

    cleanup_all-vagrant_init_destroy_boxes.sh rewritten to pass shellcheck

--- tools/cleanup_all-vagrant_init_destroy_boxes.sh
+++ tools/cleanup_all-vagrant_init_destroy_boxes.sh
@@ -4,16 +4,16 @@ set -o pipefail
 
 TMPDIR=${TMPDIR:-/var/tmp/vagrant_init_destroy_boxes}
 
-for DIR in `find $TMPDIR -maxdepth 1 -mindepth 1 -type d`; do
-  VAGRANT_BOX_NAME=$(awk -F\" '/config.vm.box =/ { print $2 }' $DIR/Vagrantfile)
+while IFS= read -r -d '' DIR; do
+  VAGRANT_BOX_NAME=$(awk -F\" '/config.vm.box =/ { print $2 }' "$DIR/Vagrantfile")
   VAGRANT_BOX_PROVIDER=${VAGRANT_BOX_NAME##*-}
 
   echo "*** $DIR | $VAGRANT_BOX_NAME | $VAGRANT_BOX_PROVIDER"
-  cd $DIR
+  cd "$DIR"
   vagrant destroy -f
   if [ "$VAGRANT_BOX_PROVIDER" = "libvirt" ]; then
-    virsh --connect=qemu:///system vol-delete --pool default --vol ${VAGRANT_BOX_NAME}_vagrant_box_image_0.img
+    virsh --connect=qemu:///system vol-delete --pool default --vol "${VAGRANT_BOX_NAME}_vagrant_box_image_0.img"
   fi
-  cd -
-  rm -rf $DIR
-done
+  cd
+  rm -rf "$DIR"
+done <   <(find "$TMPDIR" -maxdepth 1 -mindepth 1 -type d -print0)
