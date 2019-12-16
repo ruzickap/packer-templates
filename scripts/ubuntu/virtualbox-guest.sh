5cdde2e422d22408447de0bb71bc1b24954a6b86
Felipe de Cássio Rocha Santos <felipesantos@inatel.br>
2019-12-16 10:39:46 -0300

    Update VBox Guest Additions to latest version
    To do this, we switch the installation from the package manager to
    directly from the ISO.
    
    Signed-off-by: Felipe de Cássio Rocha Santos <felipesantos@inatel.br>

--- scripts/ubuntu/virtualbox-guest.sh
+++ scripts/ubuntu/virtualbox-guest.sh
@@ -6,5 +6,28 @@ if grep -q VBOX /proc/scsi/scsi; then
   echo "*** Installing virtualbox-guest-utils"
 
   export DEBIAN_FRONTEND="noninteractive"
-  apt-get install -y -qq --no-install-recommends virtualbox-guest-utils
+
+  # Method 1: from package manager
+  # apt-get install -y -qq --no-install-recommends virtualbox-guest-utils
+
+  # Method 2: from ISO
+  # Install dependencies
+  apt-get update
+  apt-get install -y curl linux-headers-$(uname -r) build-essential dkms
+
+  ## Fetch latest version
+  BASE_URL="https://download.virtualbox.org/virtualbox"
+  VERSION="$(curl -fsSL "${BASE_URL}/LATEST-STABLE.TXT")"
+
+  ## Install
+  ADDITIONS_ISO="VBoxGuestAdditions_${VERSION}.iso"
+  ADDITIONS_PATH="/media/VBoxGuestAdditions"
+  wget --quiet "${BASE_URL}/${VERSION}/${ADDITIONS_ISO}"
+  mkdir "${ADDITIONS_PATH}"
+  mount -o loop,ro "${ADDITIONS_ISO}" "${ADDITIONS_PATH}"
+  # Return code when success is 2
+  "${ADDITIONS_PATH}/VBoxLinuxAdditions.run" --nox11 || [ "$?" = 2 ] && true || false
+  rm "${ADDITIONS_ISO}"
+  umount "${ADDITIONS_PATH}"
+  rmdir "${ADDITIONS_PATH}"
 fi
