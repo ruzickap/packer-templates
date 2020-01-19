cf3eea6583d556b5b0a85ec5977dafb535468c16
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-19 20:58:24 +0100

    Disable WinRM using firewall until the OS is fully up and running
    Disable WinRM using firewall until the OS is fully up and running

--- scripts/win-common/sysprep.bat
+++ scripts/win-common/sysprep.bat
@@ -1,5 +1,13 @@
 mkdir %WINDIR%\Setup\Scripts
-echo wmic useraccount where "name='vagrant'" set passwordexpires=false > %WINDIR%\Setup\Scripts\SetupComplete.cmd
-echo wmic useraccount where "name='Administrator'" set passwordexpires=false >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
+
+rem Disable password expiration for vagrant and Administrator users
+echo wmic useraccount where "name='vagrant'" set passwordexpires=false ^>^> %%WINDIR%%\Temp\SetupComplete.log >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
+echo wmic useraccount where "name='Administrator'" set passwordexpires=false ^>^> %%WINDIR%%\Temp\SetupComplete.log >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
+
+rem Disable WinRM when until Windows is not fully initialized / started
+netsh advfirewall firewall set rule name="Allow WinRM HTTPS" new action=block
+netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new action=block
+echo netsh advfirewall firewall set rule name="Allow WinRM HTTPS" new action=allow ^>^> %%WINDIR%%\Temp\SetupComplete.log >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
+echo netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new action=allow ^>^> %%WINDIR%%\Temp\SetupComplete.log >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
 
 %WINDIR%\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:A:\unattend.xml
