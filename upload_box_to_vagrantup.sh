0354c3f9bd9ff8baa7bdff4f673ca8cf45bdf281
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-02-05 17:43:36 +0100

    Fix upload_box_to_vagrantup.sh script to upload also VirtualBox boxes
    Fix upload_box_to_vagrantup.sh script to upload also VirtualBox boxes

--- upload_box_to_vagrantup.sh
+++ upload_box_to_vagrantup.sh
@@ -167,10 +167,10 @@ create_vagrantup_box() {
 
 upload_boxfile_to_vagrantup() {
   #Get the current version before uploading anything
-  echo "*** Getting current version of the box (if exists) from Vagrant Cloud"
+  echo "*** Getting current version of the box from Vagrant Cloud"
   CURRENT_VERSION=$(curl -s "https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USER/$NAME" | jq -r ".current_version.version")
   echo "*** Current version of the box in Vagrant Cloud: $CURRENT_VERSION"
-  if [ "$CURRENT_VERSION" != "$BOX_VERSION" ]; then
+  if ( [ "$CURRENT_VERSION" == "$BOX_VERSION" ] && ! curl -s "https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USER/$NAME" | jq .versions[].providers[].name | grep -q "$VAGRANT_PROVIDER" ) || [ "$CURRENT_VERSION" != "$BOX_VERSION" ] ; then
     curl -sS "https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USER/$NAME/versions" -X POST -d "version[version]=$BOX_VERSION" -d "access_token=$VAGRANTUP_ACCESS_TOKEN" -o /dev/null
     curl -sS "https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USER/$NAME/version/$BOX_VERSION" -X PUT -d "version[description]=$LONG_DESCRIPTION" -d "access_token=$VAGRANTUP_ACCESS_TOKEN" -o /dev/null
     curl -sS "https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USER/$NAME/version/$BOX_VERSION/providers" -X POST -d "provider[name]=$VAGRANT_PROVIDER" -d "access_token=$VAGRANTUP_ACCESS_TOKEN" -o /dev/null
@@ -198,7 +198,7 @@ upload_boxfile_to_vagrantup() {
     fi
     echo "*** Done"
   else
-    echo  "*** The box with version \"$BOX_VERSION\" already exists in Vagrant Cloud. Skipping upload..."
+    echo  "*** The box with version \"$BOX_VERSION\" for \"$VAGRANT_PROVIDER\" provider already exists in Vagrant Cloud. Skipping upload..."
   fi
 }
 
