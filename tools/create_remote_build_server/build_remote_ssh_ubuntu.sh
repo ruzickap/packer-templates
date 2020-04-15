bbb676feb49ade59ccf28fc25db5c8423c0026d6
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-04-15 07:13:25 +0200

    New build_remote_ssh_ubuntu (refactoring ansible playbook)

--- tools/create_remote_build_server/build_remote_ssh_ubuntu.sh
+++ tools/create_remote_build_server/build_remote_ssh_ubuntu.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -eux
 
+# Use with Ubuntu Server 20.04
+
 REMOTE_IP="company-nb"
 REMOTE_USER="pruzicka"
+USER_PASSWORD="xxxxxxxxxx"
 MY_PACKER_TEMPLATES_GITHUB_REPOSITORY="https://github.com/ruzickap/packer-templates"
 GITHUB_SELF_HOSTED_RUNNER_TOKEN=${TOKEN:-Axxxxxxxxxxxxxxxxxxxxxxxxxxx4}
 
 # Prepare remote machine for build
 if [ -n "$GITHUB_SELF_HOSTED_RUNNER_TOKEN" ]; then
-  ansible-playbook -i "$REMOTE_IP," -e GITHUB_SELF_HOSTED_RUNNER_TOKEN="$GITHUB_SELF_HOSTED_RUNNER_TOKEN" -e MY_PACKER_TEMPLATES_GITHUB_REPOSITORY="$MY_PACKER_TEMPLATES_GITHUB_REPOSITORY" --user "$REMOTE_USER" ../ansible/build_remote_ssh_ubuntu.yml
+  ansible-playbook -i "$REMOTE_IP," -e "ansible_sudo_pass=$USER_PASSWORD" -e GITHUB_SELF_HOSTED_RUNNER_TOKEN="$GITHUB_SELF_HOSTED_RUNNER_TOKEN" -e MY_PACKER_TEMPLATES_GITHUB_REPOSITORY="$MY_PACKER_TEMPLATES_GITHUB_REPOSITORY" --user "$REMOTE_USER" build_remote_ssh_ubuntu.yml
 else
   echo "Missing GITHUB_SELF_HOSTED_RUNNER_TOKEN"
   exit 1
