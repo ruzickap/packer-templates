d82f8b88790edd0f9179ea04932a797a6a6bd07c
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-04-18 09:50:34 +0200

    Make apt-get commands silent

--- scripts/ubuntu/virtualbox-guest-x11.sh
+++ scripts/ubuntu/virtualbox-guest-x11.sh
@@ -6,5 +6,5 @@ if grep -q VBOX /proc/scsi/scsi; then
   echo "*** Installing virtualbox-guest-x11"
 
   export DEBIAN_FRONTEND="noninteractive"
-  apt-get install -y -qq --no-install-recommends virtualbox-guest-x11
+  apt-get install -y -qq --no-install-recommends virtualbox-guest-x11 > /dev/null
 fi
--- scripts/ubuntu/virtualbox-guest.sh
+++ scripts/ubuntu/virtualbox-guest.sh
@@ -6,5 +6,5 @@ if grep -q VBOX /proc/scsi/scsi; then
   echo "*** Installing virtualbox-guest-utils"
 
   export DEBIAN_FRONTEND="noninteractive"
-  apt-get install -y -qq --no-install-recommends virtualbox-guest-utils
+  apt-get install -y -qq --no-install-recommends virtualbox-guest-utils > /dev/null
 fi
