c87f179b1901be20a35d52efc6696dfe744f8dad
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-04-22 09:00:02 +0200

    Remove TMPDIR variable and use PACKER_CACHE_DIR instead

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eu
 
-# Temporary directory where to store the iso images and other packer files
-export TMPDIR=${TMPDIR:-$PWD/packer_cache}
+# Packer cache directory (where to store the iso images)
+export PACKER_CACHE_DIR=${PACKER_CACHE_DIR:-${PWD}/packer_cache}
 # VirtIO win iso URL (https://www.linux-kvm.org/page/WindowsGuestDrivers/Download_Drivers)
 export VIRTIO_WIN_ISO_URL=${VIRTIO_WIN_ISO_URL:-https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win.iso}
-export VIRTIO_WIN_ISO=${VIRTIO_WIN_ISO:-$TMPDIR/$(basename "$VIRTIO_WIN_ISO_URL")}
+export VIRTIO_WIN_ISO=${VIRTIO_WIN_ISO:-${PACKER_CACHE_DIR}/$(basename "${VIRTIO_WIN_ISO_URL}")}
 # Do not use any GUI X11 windows
 export HEADLESS=${HEADLESS:-true}
 # Use packer, virtualboc, ansible in docker image
@@ -14,7 +14,7 @@ export PACKER_BINARY=${PACKER_BINARY:-packer}
 # Directory where all the images will be stored
 export PACKER_IMAGES_OUTPUT_DIR=${PACKER_IMAGES_OUTPUT_DIR:-/var/tmp/packer-templates-images}
 # Directory where to store the logs
-export LOGDIR=${LOGDIR:-$PACKER_IMAGES_OUTPUT_DIR}
+export LOGDIR=${LOGDIR:-${PACKER_IMAGES_OUTPUT_DIR}}
 # Enable packer debug log if set to 1 (default 0)
 export PACKER_LOG=${PACKER_LOG:-0}
 # Max amount of time which packer can run (default 5 hours) - this prevent packer form running forever when something goes bad during provisioning/build process
@@ -35,7 +35,7 @@ set -o pipefail
 usage() {
   cat <<- EOF
 
-Usage: $PROGNAME options
+Usage: ${PROGNAME} options
 
 This script can build the various libvirt and virtualbox images.
 You should have Packer, Ansible, libvirt and VirtualBox installed.
@@ -59,7 +59,7 @@ List of all supported builds:
 Examples:
 
 Build Windows 10 Enterprise Evaluation, Windows Server 2019 Standard Evaluation, Windows Server 2016 Standard Evaluation and Windows Server 2012 Standard Evaluation for Virtualbox and libvirt:
-  $PROGNAME \\
+  ${PROGNAME} \\
     my_windows-10-enterprise-x64-eval-{libvirt,virtualbox} \\
     windows-10-enterprise-x64-eval-{libvirt,virtualbox} \\
     windows-server-2019-standard-x64-eval-{libvirt,virtualbox} \\
@@ -67,7 +67,7 @@ Build Windows 10 Enterprise Evaluation, Windows Server 2019 Standard Evaluation,
     windows-server-2012_r2-standard-x64-eval-{libvirt,virtualbox}
 
 Build Ubuntu Desktop 20.04, 18.04; Ubuntu Server 20.04, 18.04, 16.04; My Ubuntu Server 20.04, 18.04, 16.04; My CentOS 7 for libvirt and Virtualbox:
-  $PROGNAME \\
+  ${PROGNAME} \\
     ubuntu-{20.04,18.04}-desktop-amd64-{libvirt,virtualbox} \\
     ubuntu-{20.04,18.04,16.04}-server-amd64-{libvirt,virtualbox} \\
     my_ubuntu-{20.04,18.04,16.04}-server-amd64-{libvirt,virtualbox} \\
@@ -78,18 +78,18 @@ EOF
 cmdline() {
   BUILDS=$*
 
-  if [[ -z "$BUILDS" ]]; then
+  if [[ -z "${BUILDS}" ]]; then
     usage
     exit 0;
   fi
 
-  for BUILD in $BUILDS; do
+  for BUILD in ${BUILDS}; do
     export PACKER_VAGRANT_PROVIDER="${BUILD##*-}"
     export NAME="${BUILD%-*}"
-    MY_NAME=$(echo "$NAME" | awk -F '-' '{ print $1 }')
+    MY_NAME=$(echo "${NAME}" | awk -F '-' '{ print $1 }')
     export MY_NAME
 
-    case $PACKER_VAGRANT_PROVIDER in
+    case ${PACKER_VAGRANT_PROVIDER} in
       libvirt )
         export PACKER_BUILDER_TYPE="qemu"
       ;;
@@ -97,33 +97,33 @@ cmdline() {
         export PACKER_BUILDER_TYPE="virtualbox-iso"
       ;;
       *)
-        echo -e "\n\n*** Unsupported PACKER_VAGRANT_PROVIDER: \"$PACKER_VAGRANT_PROVIDER\" used from \"$BUILD\""
+        echo -e "\n\n*** Unsupported PACKER_VAGRANT_PROVIDER: \"${PACKER_VAGRANT_PROVIDER}\" used from \"${BUILD}\""
         exit 1
       ;;
     esac
 
-    test -d "$TMPDIR"                   || mkdir -v "$TMPDIR"
-    test -d "$PACKER_IMAGES_OUTPUT_DIR" || mkdir -v "$PACKER_IMAGES_OUTPUT_DIR"
-    test -d "$LOGDIR"                   || mkdir -v "$LOGDIR"
+    test -d "${PACKER_CACHE_DIR}"         || mkdir -v "${PACKER_CACHE_DIR}"
+    test -d "${PACKER_IMAGES_OUTPUT_DIR}" || mkdir -v "${PACKER_IMAGES_OUTPUT_DIR}"
+    test -d "${LOGDIR}"                   || mkdir -v "${LOGDIR}"
 
-    echo -e "\n\n*** $MY_NAME | $NAME | $BUILD - $PACKER_VAGRANT_PROVIDER/$PACKER_BUILDER_TYPE"
+    echo -e "\n\n*** ${MY_NAME} | ${NAME} | ${BUILD} - ${PACKER_VAGRANT_PROVIDER}/${PACKER_BUILDER_TYPE}"
 
-    case $NAME in
+    case ${NAME} in
       *centos*)
-        CENTOS_VERSION=$(echo "$NAME" | awk -F '-' '{ print $2 }')
+        CENTOS_VERSION=$(echo "${NAME}" | awk -F '-' '{ print $2 }')
         export CENTOS_VERSION
-        CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/$CENTOS_VERSION/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
+        CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
         export CENTOS_TAG
         export CENTOS_TYPE="NetInstall"
-        ISO_CHECKSUM=$(curl -s "ftp://ftp.cvut.cz/centos/$CENTOS_VERSION/isos/x86_64/sha256sum.txt" | awk "/CentOS-${CENTOS_VERSION}-x86_64-${CENTOS_TYPE}-${CENTOS_TAG}.iso/ { print \$1 }")
+        ISO_CHECKSUM=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | awk "/CentOS-${CENTOS_VERSION}-x86_64-${CENTOS_TYPE}-${CENTOS_TAG}.iso/ { print \$1 }")
         export PACKER_FILE="${MY_NAME}-${CENTOS_VERSION}.json"
         export DOCKER_ENV_PARAMETERS="-e CENTOS_VERSION -e CENTOS_TAG -e CENTOS_TYPE -e NAME"
-        echo "* NAME: $NAME, CENTOS_VERSION: $CENTOS_VERSION, CENTOS_TAG: $CENTOS_TAG, CENTOS_TYPE: $CENTOS_TYPE, PACKER_FILE: $PACKER_FILE "
+        echo "* NAME: ${NAME}, CENTOS_VERSION: ${CENTOS_VERSION}, CENTOS_TAG: ${CENTOS_TAG}, CENTOS_TYPE: ${CENTOS_TYPE}, PACKER_FILE: ${PACKER_FILE}"
       ;;
       *ubuntu*)
-        UBUNTU_TYPE=$(echo "$NAME" | awk -F '-' '{ print $3 }')
+        UBUNTU_TYPE=$(echo "${NAME}" | awk -F '-' '{ print $3 }')
         export UBUNTU_TYPE
-        UBUNTU_VERSION=$(echo "$NAME" | awk -F '-' '{ print $2 }')
+        UBUNTU_VERSION=$(echo "${NAME}" | awk -F '-' '{ print $2 }')
         export UBUNTU_VERSION
         UBUNTU_CODENAME=$(curl -s http://releases.ubuntu.com/ | sed -n "s@.*<a href=\"\([a-z]*\)/\">.*Ubuntu ${UBUNTU_VERSION}.*@\1@p" | head -1)
         if wget -q -O/dev/null "http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}-updates/main/installer-amd64/current/images/SHA256SUMS" ; then
@@ -140,13 +140,13 @@ cmdline() {
       ;;
       *windows*)
         export WINDOWS_ARCH="x64"
-        WINDOWS_VERSION=$(echo "$NAME" | sed -n -e 's/.*-\([0-9][0-9][0-9][0-9]\)[_-].*/\1/p' -e 's/.*-\([0-9][0-9]\)-.*/\1/p')
+        WINDOWS_VERSION=$(echo "${NAME}" | sed -n -e 's/.*-\([0-9][0-9][0-9][0-9]\)[_-].*/\1/p' -e 's/.*-\([0-9][0-9]\)-.*/\1/p')
         export WINDOWS_VERSION
         export PACKER_FILE="${MY_NAME}.json"
-        WINDOWS_EDITION=$(echo "$NAME" | awk -F - '{ print $(NF-2) }')
+        WINDOWS_EDITION=$(echo "${NAME}" | awk -F - '{ print $(NF-2) }')
         export WINDOWS_EDITION
 
-        case $NAME in
+        case ${NAME} in
           *windows-10-enterprise*)
             export ISO_URL="https://software-download.microsoft.com/download/pr/18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
           ;;
@@ -164,19 +164,19 @@ cmdline() {
             export ISO_URL="http://download.microsoft.com/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO"
           ;;
           *)
-            echo "*** Unsupported Windows build type: \"$NAME\" used from \"$BUILD\""
+            echo "*** Unsupported Windows build type: \"${NAME}\" used from \"${BUILD}\""
             exit 1
           ;;
         esac
 
-        echo "* NAME: $NAME, WINDOWS_ARCH: $WINDOWS_ARCH, WINDOWS_VERSION: $WINDOWS_VERSION, WINDOWS_EDITION: $WINDOWS_EDITION, PACKER_FILE: $PACKER_FILE"
+        echo "* NAME: ${NAME}, WINDOWS_ARCH: ${WINDOWS_ARCH}, WINDOWS_VERSION: ${WINDOWS_VERSION}, WINDOWS_EDITION: ${WINDOWS_EDITION}, PACKER_FILE: ${PACKER_FILE}"
         ISO_CHECKSUM=$(awk "/$(basename ${ISO_URL})/ { print \$1 }" win_iso.sha256)
-        test -f "$VIRTIO_WIN_ISO" || wget --continue "$VIRTIO_WIN_ISO_URL" -O "$VIRTIO_WIN_ISO"
-        DOCKER_ENV_PARAMETERS="-e WINDOWS_VERSION -e NAME -e ISO_URL -e VIRTIO_WIN_ISO=packer_cache/$(basename "$VIRTIO_WIN_ISO")"
+        test -f "${VIRTIO_WIN_ISO}" || wget --continue "${VIRTIO_WIN_ISO_URL}" -O "${VIRTIO_WIN_ISO}"
+        DOCKER_ENV_PARAMETERS="-e WINDOWS_VERSION -e NAME -e ISO_URL -e VIRTIO_WIN_ISO=packer_cache/$(basename "${VIRTIO_WIN_ISO}")"
         export DOCKER_ENV_PARAMETERS
       ;;
       *)
-        echo "*** Unsupported build type: \"$NAME\" used from \"$BUILD\""
+        echo "*** Unsupported build type: \"${NAME}\" used from \"${BUILD}\""
         exit 1
       ;;
     esac
@@ -189,20 +189,20 @@ cmdline() {
 
 packer_build() {
   if [[ ! -f "${PACKER_IMAGES_OUTPUT_DIR}/${BUILD}.box" ]]; then
-    if [[ "$USE_DOCKERIZED_PACKER" = "true" ]]; then
-      $DOCKER_COMMAND pull peru/packer_qemu_virtualbox_ansible
-      $DOCKER_COMMAND run --rm -t -u "$(id -u):$(id -g)" --privileged --tmpfs /dev/shm:size=67108864 --network host --name "packer_${BUILD}" "$DOCKER_ENV_PARAMETERS" \
-        -v "$PACKER_IMAGES_OUTPUT_DIR:/home/docker/packer_images_output_dir" \
-        -v "$PWD:/home/docker/packer" \
-        -v "$TMPDIR:/home/docker/packer/packer_cache" \
+    if [[ "${USE_DOCKERIZED_PACKER}" = "true" ]]; then
+      ${DOCKER_COMMAND} pull peru/packer_qemu_virtualbox_ansible
+      ${DOCKER_COMMAND} run --rm -t -u "$(id -u):$(id -g)" --privileged --tmpfs /dev/shm:size=67108864 --network host --name "packer_${BUILD}" "${DOCKER_ENV_PARAMETERS}" \
+        -v "${PACKER_IMAGES_OUTPUT_DIR}:/home/docker/packer_images_output_dir" \
+        -v "${PWD}:/home/docker/packer" \
+        -v "${PACKER_CACHE_DIR}:/home/docker/packer/packer_cache" \
         -e PACKER_RUN_TIMEOUT \
         -e PACKER_LOG \
         -e PACKER_IMAGES_OUTPUT_DIR=/home/docker/packer_images_output_dir \
-        peru/packer_qemu_virtualbox_ansible build -only="$PACKER_BUILDER_TYPE" -color=false -var "headless=$HEADLESS" "$PACKER_FILE" 2>&1 | tee "${LOGDIR}/${BUILD}-packer.log"
+        peru/packer_qemu_virtualbox_ansible build -only="${PACKER_BUILDER_TYPE}" -color=false -var "headless=${HEADLESS}" "${PACKER_FILE}" 2>&1 | tee "${LOGDIR}/${BUILD}-packer.log"
     else
-      $PACKER_BINARY build -only="$PACKER_BUILDER_TYPE" -color=false -var "headless=$HEADLESS" "$PACKER_FILE" 2>&1 | tee "${LOGDIR}/${BUILD}-packer.log"
+      ${PACKER_BINARY} build -only="${PACKER_BUILDER_TYPE}" -color=false -var "headless=${HEADLESS}" "${PACKER_FILE}" 2>&1 | tee "${LOGDIR}/${BUILD}-packer.log"
     fi
-    [[ -L "${TMPDIR}/${NAME}.iso" ]] || ln -rvs "${TMPDIR}/$(echo -n "${ISO_CHECKSUM}" | sha1sum | awk '{ print $1 }').iso" "${TMPDIR}/${NAME}.iso"
+    [[ -L "${PACKER_CACHE_DIR}/${NAME}.iso" ]] || ln -rvs "${PACKER_CACHE_DIR}/$(echo -n "${ISO_CHECKSUM}" | sha1sum | awk '{ print $1 }').iso" "${PACKER_CACHE_DIR}/${NAME}.iso"
   else
     echo -e "\n* File ${PACKER_IMAGES_OUTPUT_DIR}/${BUILD}.box already exists. Skipping....\n";
   fi
@@ -215,7 +215,7 @@ packer_build() {
 #######
 
 main() {
-  cmdline "$ARGS"
+  cmdline "${ARGS}"
 }
 
 main
