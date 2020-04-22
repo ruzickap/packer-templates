06b5c16cb8de7f1434b3a1cd647549f924f4fa2d
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-04-22 09:10:36 +0200

    Remove LOGDIR variable from upload_box_to_vagrantcloud.sh script

--- upload_box_to_vagrantcloud.sh
+++ upload_box_to_vagrantcloud.sh
@@ -3,7 +3,6 @@
 set -o pipefail
 
 export BOX_VERSION=${BOX_VERSION:-$(date +%Y%m%d).01}
-export LOGDIR=${LOGDIR:-/var/tmp}
 
 readonly PROGNAME=$(basename "$0")
 readonly ARGS=$*
@@ -12,7 +11,7 @@ readonly ARGS=$*
 usage() {
   cat <<- EOF
 
-Usage: $PROGNAME options
+Usage: ${PROGNAME} options
 
 This script can upload Packer box images to Vagrant Cloud.
 
@@ -40,40 +39,40 @@ Examples:
 Upload the "windows-server-2012_r2-standard-x64-eval-virtualbox.box" to peru/windows-server-2012_r2-standard-x64-eval:
   export VAGRANT_CLOUD_TOKEN="123456"
 
-  $PROGNAME <vagrant_cloud_user>@<box_image>
-  $PROGNAME peru@windows-server-2012_r2-standard-x64-eval-virtualbox.box
+  ${PROGNAME} <vagrant_cloud_user>@<box_image>
+  ${PROGNAME} peru@windows-server-2012_r2-standard-x64-eval-virtualbox.box
 EOF
 }
 
 cmdline() {
   local USER_BOX=$*
 
-  if [[ -z "$USER_BOX" ]] || [[ -z "$VAGRANT_CLOUD_TOKEN" ]]; then
+  if [[ -z "${USER_BOX}" ]] || [[ -z "${VAGRANT_CLOUD_TOKEN}" ]]; then
     usage
     exit 1;
   fi
 
   export VAGRANT_CLOUD_USER="${USER_BOX%@*}"
   VAGRANT_CLOUD_BOX_FILE="${USER_BOX##*@}"
-  VAGRANT_CLOUD_BOX_NAME=$(basename "$VAGRANT_CLOUD_BOX_FILE" .box)
-  MY_NAME=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $1 }')
-  VAGRANT_PROVIDER=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $NF }')
+  VAGRANT_CLOUD_BOX_NAME=$(basename "${VAGRANT_CLOUD_BOX_FILE}" .box)
+  MY_NAME=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $1 }')
+  VAGRANT_PROVIDER=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $NF }')
   export VAGRANT_PROVIDER
 
-  if [[ ! -f "$VAGRANT_CLOUD_BOX_FILE" ]]; then
-    echo -e "*** ERROR: \"$VAGRANT_CLOUD_BOX_FILE\" does not exist!\n"
+  if [[ ! -f "${VAGRANT_CLOUD_BOX_FILE}" ]]; then
+    echo -e "*** ERROR: \"${VAGRANT_CLOUD_BOX_FILE}\" does not exist!\n"
     exit 1
   fi
 
-  echo "*** My Name: $MY_NAME | User: $VAGRANT_CLOUD_USER  | Provider: $VAGRANT_PROVIDER | Box file: $VAGRANT_CLOUD_BOX_FILE | Box name: $VAGRANT_CLOUD_BOX_NAME"
+  echo "*** My Name: ${MY_NAME} | User: ${VAGRANT_CLOUD_USER}  | Provider: ${VAGRANT_PROVIDER} | Box file: ${VAGRANT_CLOUD_BOX_FILE} | Box name: ${VAGRANT_CLOUD_BOX_NAME}"
 
-  case $VAGRANT_CLOUD_BOX_NAME in
+  case ${VAGRANT_CLOUD_BOX_NAME} in
     *centos*)
-      CENTOS_VERSION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $2 }')
+      CENTOS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
       export CENTOS_VERSION
-      CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/$CENTOS_VERSION/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
+      CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
       export CENTOS_TAG
-      CENTOS_ARCH=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $3 }')
+      CENTOS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
       export CENTOS_ARCH
       export CENTOS_TYPE="NetInstall"
       export NAME="${MY_NAME}-${CENTOS_VERSION}-${CENTOS_ARCH}"
@@ -82,12 +81,12 @@ cmdline() {
       export LONG_DESCRIPTION
     ;;
     *ubuntu*)
-      UBUNTU_TYPE=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $3 }')
+      UBUNTU_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
       export UBUNTU_TYPE
       export UBUNTU_TYPE_UC=${UBUNTU_TYPE^}
-      UBUNTU_MAJOR_VERSION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $2 }')
+      UBUNTU_MAJOR_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
       export UBUNTU_MAJOR_VERSION
-      UBUNTU_ARCH=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $4 }')
+      UBUNTU_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
       export UBUNTU_ARCH
       UBUNTU_VERSION=$(curl -s "http://releases.ubuntu.com/${UBUNTU_MAJOR_VERSION}/SHA1SUMS" | sed -n "s/.*ubuntu-\([^-]*\)-.*-${UBUNTU_ARCH}.iso/\1/p" | head -1)
       export UBUNTU_VERSION
@@ -97,50 +96,50 @@ cmdline() {
       export LONG_DESCRIPTION
     ;;
     *windows-10*)
-      WINDOWS_VERSION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $2 }')
+      WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
       export WINDOWS_VERSION
-      WINDOWS_ARCH=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $4 }')
+      WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
       export WINDOWS_ARCH
-      WINDOWS_EDITION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $3 }')
+      WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
       export WINDOWS_EDITION
       export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
       export NAME="${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-      export SHORT_DESCRIPTION="Windows $WINDOWS_VERSION ${WINDOWS_EDITION_UC} ($WINDOWS_ARCH) Evaluation for libvirt and virtualbox"
+      export SHORT_DESCRIPTION="Windows ${WINDOWS_VERSION} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
       LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-eval.md")
       export LONG_DESCRIPTION
     ;;
     *windows-*-2012*)
-      WINDOWS_VERSION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '[-_]' '{ print $3 }')
+      WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '[-_]' '{ print $3 }')
       export WINDOWS_VERSION
-      WINDOWS_RELEASE=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '[-_]' '{ print $4 }')
+      WINDOWS_RELEASE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '[-_]' '{ print $4 }')
       export WINDOWS_RELEASE
       export WINDOWS_RELEASE_UC=${WINDOWS_RELEASE^}
-      WINDOWS_ARCH=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $5 }')
+      WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $5 }')
       export WINDOWS_ARCH
-      WINDOWS_TYPE=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $2 }')
+      WINDOWS_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
       export WINDOWS_TYPE
       export WINDOWS_TYPE_UC=${WINDOWS_TYPE^}
-      WINDOWS_EDITION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $4 }')
+      WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
       export WINDOWS_EDITION
       export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
       export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}_${WINDOWS_RELEASE}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-      export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} $WINDOWS_VERSION ${WINDOWS_RELEASE_UC} ${WINDOWS_EDITION_UC} ($WINDOWS_ARCH) Evaluation for libvirt and virtualbox"
+      export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} ${WINDOWS_VERSION} ${WINDOWS_RELEASE_UC} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
       LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.md")
       export LONG_DESCRIPTION
     ;;
     *windows-*-201[69]*)
-      WINDOWS_VERSION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $3 }')
+      WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
       export WINDOWS_VERSION
-      WINDOWS_ARCH=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $5 }')
+      WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $5 }')
       export WINDOWS_ARCH
-      WINDOWS_TYPE=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $2 }')
+      WINDOWS_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
       export WINDOWS_TYPE
       export WINDOWS_TYPE_UC=${WINDOWS_TYPE^}
-      WINDOWS_EDITION=$(echo "$VAGRANT_CLOUD_BOX_NAME" | awk -F '-' '{ print $4 }')
+      WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
       export WINDOWS_EDITION
       export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
       export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-      export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} $WINDOWS_VERSION ${WINDOWS_EDITION_UC} ($WINDOWS_ARCH) Evaluation for libvirt and virtualbox"
+      export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} ${WINDOWS_VERSION} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
       LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.md")
       export LONG_DESCRIPTION
     ;;
@@ -149,7 +148,7 @@ cmdline() {
   echo -e "*** ${NAME} | ${SHORT_DESCRIPTION}"
   CHECKSUM_BOX_FILE=$(sha256sum "${VAGRANT_CLOUD_BOX_FILE}" | cut -d ' ' -f 1)
 
-  if curl --silent --fail "https://vagrantcloud.com/api/v1/box/$VAGRANT_CLOUD_USER/$NAME" -o /dev/null; then
+  if curl --silent --fail "https://vagrantcloud.com/api/v1/box/${VAGRANT_CLOUD_USER}/${NAME}" -o /dev/null; then
     echo "*** Box: ${NAME} - already exists..."
     CHECKSUM_BOX_VAGRANT_CLOUD=$(curl -s "https://app.vagrantup.com/${VAGRANT_CLOUD_USER}/boxes/${NAME}" | jq -r ".versions[] | select (.version == \"${BOX_VERSION}\") .providers[] | select (.name == \"${VAGRANT_PROVIDER}\") .checksum")
 
@@ -162,7 +161,7 @@ cmdline() {
     echo "*** Box: ${NAME} - doesn't exist..."
   fi
 
-  vagrant cloud publish --force --description "$LONG_DESCRIPTION" --version-description "$LONG_DESCRIPTION" --release  --short-description "${SHORT_DESCRIPTION}" --checksum-type sha256 --checksum "${CHECKSUM_BOX_FILE}" "${VAGRANT_CLOUD_USER}/${NAME}" "${BOX_VERSION}" "${VAGRANT_PROVIDER}" "${VAGRANT_CLOUD_BOX_FILE}"
+  vagrant cloud publish --force --description "${LONG_DESCRIPTION}" --version-description "${LONG_DESCRIPTION}" --release  --short-description "${SHORT_DESCRIPTION}" --checksum-type sha256 --checksum "${CHECKSUM_BOX_FILE}" "${VAGRANT_CLOUD_USER}/${NAME}" "${BOX_VERSION}" "${VAGRANT_PROVIDER}" "${VAGRANT_CLOUD_BOX_FILE}"
 }
 
 
@@ -171,7 +170,7 @@ cmdline() {
 #######
 
 main() {
-  cmdline "$ARGS"
+  cmdline "${ARGS}"
 }
 
 main
