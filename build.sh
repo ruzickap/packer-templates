4b54167e6910e3b8a44f1c7bb704dabbb24ce239
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-04-24 04:21:30 +0200

    Fix typo in variable in build.sh

--- build.sh
+++ build.sh
@@ -202,7 +202,7 @@ packer_build() {
     else
       ${PACKER_BINARY} build -only="${PACKER_BUILDER_TYPE}" -color=false -var "headless=${HEADLESS}" "${PACKER_FILE}" 2>&1 | tee "${LOGDIR}/${BUILD}-packer.log"
     fi
-    ln -rfs "${VAGRANT_ISO_FILE}" "${PACKER_CACHE_DIR}/${NAME}.iso"
+    ln -rfs "${PACKER_CACHE_DIR}/$(echo -n "${ISO_CHECKSUM}" | sha1sum | awk '{ print $1 }').iso" "${PACKER_CACHE_DIR}/${NAME}.iso"
   else
     echo -e "\n* File ${PACKER_IMAGES_OUTPUT_DIR}/${BUILD}.box already exists. Skipping....\n";
   fi
