ed643633302c619848d3e74aeea75edc8a965d60
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-09-18 21:24:16 +0200

    Fix bash issue on MacOS during GH Action build process

--- vagrant_init_destroy_boxes.sh
+++ vagrant_init_destroy_boxes.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -eu
+#!/usr/bin/env bash
 
-set -o pipefail
+set -eu -o pipefail
 
 BOXES_LIST=${*:-$(find . -maxdepth 1 \( -name "*ubuntu*.box" -o -name "*centos*.box" -o -name "*windows*.box" \) -printf "%f\n" | sort | tr "\n" " ")}
 TMPDIR=${TMPDIR:-/tmp}
