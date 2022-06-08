eb8ae4408b5ce0ae904fbe937f55cd2db3c75a11
Petr Ruzicka <petr.ruzicka@gmail.com>
2022-06-08 18:54:15 +0200

    Fix linter issues (#401)

--- build.sh
+++ build.sh
@@ -70,7 +70,7 @@ cmdline() {
 
   if [[ -z "${BUILDS}" ]]; then
     usage
-    exit 0;
+    exit 0
   fi
 
   for BUILD in ${BUILDS}; do
@@ -82,101 +82,101 @@ cmdline() {
     export MY_NAME
 
     case ${PACKER_VAGRANT_PROVIDER} in
-      libvirt )
-        # Qemu Accelerator - use kvm for Linux and hvf for MacOS
-        if [[ $(uname) = "Darwin" ]]; then
-          PACKER_CMD_PARAMS+=("-only=qemu" "-var" "accelerator=hvf")
-        elif [[ $(uname) = "Linux" ]]; then
-          PACKER_CMD_PARAMS+=("-only=qemu" "-var" "accelerator=kvm")
-        fi
+    libvirt)
+      # Qemu Accelerator - use kvm for Linux and hvf for MacOS
+      if [[ $(uname) = "Darwin" ]]; then
+        PACKER_CMD_PARAMS+=("-only=qemu" "-var" "accelerator=hvf")
+      elif [[ $(uname) = "Linux" ]]; then
+        PACKER_CMD_PARAMS+=("-only=qemu" "-var" "accelerator=kvm")
+      fi
 
       ;;
-      virtualbox )
-        PACKER_CMD_PARAMS+=("-only=virtualbox-iso")
+    virtualbox)
+      PACKER_CMD_PARAMS+=("-only=virtualbox-iso")
       ;;
-      *)
-        echo -e "\n\n*** Unsupported PACKER_VAGRANT_PROVIDER: \"${PACKER_VAGRANT_PROVIDER}\" used from \"${BUILD}\""
-        exit 1
+    *)
+      echo -e "\n\n*** Unsupported PACKER_VAGRANT_PROVIDER: \"${PACKER_VAGRANT_PROVIDER}\" used from \"${BUILD}\""
+      exit 1
       ;;
     esac
 
-    test -d "${PACKER_CACHE_DIR}"         || mkdir -v "${PACKER_CACHE_DIR}"
+    test -d "${PACKER_CACHE_DIR}" || mkdir -v "${PACKER_CACHE_DIR}"
     test -d "${PACKER_IMAGES_OUTPUT_DIR}" || mkdir -v "${PACKER_IMAGES_OUTPUT_DIR}"
-    test -d "${LOGDIR}"                   || mkdir -v "${LOGDIR}"
+    test -d "${LOGDIR}" || mkdir -v "${LOGDIR}"
 
     echo -e "\n\n*** ${MY_NAME} | ${NAME} | ${BUILD} - ${PACKER_VAGRANT_PROVIDER}"
 
     case ${NAME} in
-      *centos*)
-        CENTOS_VERSION=$(echo "${NAME}" | awk -F '-' '{ print $2 }')
-        export CENTOS_VERSION
-        CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
-        export CENTOS_TAG
-        export CENTOS_TYPE="NetInstall"
-        ISO_CHECKSUM=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | awk "/CentOS-${CENTOS_VERSION}-x86_64-${CENTOS_TYPE}-${CENTOS_TAG}.iso/ { print \$1 }")
-        PACKER_CMD_PARAMS+=("${MY_NAME}-${CENTOS_VERSION}.json")
-        echo "* NAME: ${NAME}, CENTOS_VERSION: ${CENTOS_VERSION}, CENTOS_TAG: ${CENTOS_TAG}, CENTOS_TYPE: ${CENTOS_TYPE}"
+    *centos*)
+      CENTOS_VERSION=$(echo "${NAME}" | awk -F '-' '{ print $2 }')
+      export CENTOS_VERSION
+      CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
+      export CENTOS_TAG
+      export CENTOS_TYPE="NetInstall"
+      ISO_CHECKSUM=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | awk "/CentOS-${CENTOS_VERSION}-x86_64-${CENTOS_TYPE}-${CENTOS_TAG}.iso/ { print \$1 }")
+      PACKER_CMD_PARAMS+=("${MY_NAME}-${CENTOS_VERSION}.json")
+      echo "* NAME: ${NAME}, CENTOS_VERSION: ${CENTOS_VERSION}, CENTOS_TAG: ${CENTOS_TAG}, CENTOS_TYPE: ${CENTOS_TYPE}"
       ;;
-      *ubuntu*)
-        UBUNTU_TYPE=$(echo "${NAME}" | awk -F '-' '{ print $3 }')
-        export UBUNTU_TYPE
-        UBUNTU_VERSION=$(echo "${NAME}" | awk -F '-' '{ print $2 }')
-        export UBUNTU_VERSION
-        UBUNTU_CODENAME=$(curl -s http://releases.ubuntu.com/ | sed -n "s@.*<a href=\"\([a-z]*\)/\">.*Ubuntu ${UBUNTU_VERSION}.*@\1@p" | head -1)
-        if curl --fail --silent --head --output /dev/null "http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}-updates/main/installer-amd64/current/images/SHA256SUMS" ; then
-          export UBUNTU_IMAGES_URL=http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}-updates/main/installer-amd64/current/images
-        elif curl --fail --silent --head --output /dev/null "http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}/main/installer-amd64/current/legacy-images/SHA256SUMS" ; then
-          export UBUNTU_IMAGES_URL=http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}/main/installer-amd64/current/legacy-images
-        else
-          export UBUNTU_IMAGES_URL=http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}/main/installer-amd64/current/images
-        fi
-        ISO_CHECKSUM=$(curl -s "${UBUNTU_IMAGES_URL}/SHA256SUMS" | awk '/.\/netboot\/mini.iso/ { print $1 }')
-        PACKER_CMD_PARAMS+=("${MY_NAME}-${UBUNTU_TYPE}.json")
-        echo "* NAME: ${NAME}, UBUNTU_TYPE: ${UBUNTU_TYPE}, UBUNTU_IMAGES_URL: ${UBUNTU_IMAGES_URL}"
+    *ubuntu*)
+      UBUNTU_TYPE=$(echo "${NAME}" | awk -F '-' '{ print $3 }')
+      export UBUNTU_TYPE
+      UBUNTU_VERSION=$(echo "${NAME}" | awk -F '-' '{ print $2 }')
+      export UBUNTU_VERSION
+      UBUNTU_CODENAME=$(curl -s http://releases.ubuntu.com/ | sed -n "s@.*<a href=\"\([a-z]*\)/\">.*Ubuntu ${UBUNTU_VERSION}.*@\1@p" | head -1)
+      if curl --fail --silent --head --output /dev/null "http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}-updates/main/installer-amd64/current/images/SHA256SUMS"; then
+        export UBUNTU_IMAGES_URL=http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}-updates/main/installer-amd64/current/images
+      elif curl --fail --silent --head --output /dev/null "http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}/main/installer-amd64/current/legacy-images/SHA256SUMS"; then
+        export UBUNTU_IMAGES_URL=http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}/main/installer-amd64/current/legacy-images
+      else
+        export UBUNTU_IMAGES_URL=http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}/main/installer-amd64/current/images
+      fi
+      ISO_CHECKSUM=$(curl -s "${UBUNTU_IMAGES_URL}/SHA256SUMS" | awk '/.\/netboot\/mini.iso/ { print $1 }')
+      PACKER_CMD_PARAMS+=("${MY_NAME}-${UBUNTU_TYPE}.json")
+      echo "* NAME: ${NAME}, UBUNTU_TYPE: ${UBUNTU_TYPE}, UBUNTU_IMAGES_URL: ${UBUNTU_IMAGES_URL}"
       ;;
-      *windows*)
-        export WINDOWS_ARCH="x64"
-        WINDOWS_VERSION=$(echo "${NAME}" | sed -n -e 's/.*-\([0-9][0-9][0-9][0-9]\)[_-].*/\1/p' -e 's/.*-\([0-9][0-9]\)-.*/\1/p')
-        export WINDOWS_VERSION
-        PACKER_CMD_PARAMS+=("${MY_NAME}.json")
-        WINDOWS_EDITION=$(echo "${NAME}" | awk -F - '{ print $(NF-2) }')
-        export WINDOWS_EDITION
+    *windows*)
+      export WINDOWS_ARCH="x64"
+      WINDOWS_VERSION=$(echo "${NAME}" | sed -n -e 's/.*-\([0-9][0-9][0-9][0-9]\)[_-].*/\1/p' -e 's/.*-\([0-9][0-9]\)-.*/\1/p')
+      export WINDOWS_VERSION
+      PACKER_CMD_PARAMS+=("${MY_NAME}.json")
+      WINDOWS_EDITION=$(echo "${NAME}" | awk -F - '{ print $(NF-2) }')
+      export WINDOWS_EDITION
 
-        case ${NAME} in
-          *windows-10-enterprise*)
-            export ISO_URL="https://software-download.microsoft.com/download/sg/444969d5-f34g-4e03-ac9d-1f9786c69161/19044.1288.211006-0501.21h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
-          ;;
-          *windows-server-2022-*)
-            export WINDOWS_TYPE="server"
-            export ISO_URL="https://software-download.microsoft.com/download/sg/20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
-          ;;
-          *windows-server-2019-*)
-            export WINDOWS_TYPE="server"
-            export ISO_URL="https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso"
-          ;;
-          *windows-server-2016-*)
-            export WINDOWS_TYPE="server"
-            export ISO_URL="https://software-download.microsoft.com/download/pr/Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO"
-          ;;
-          *)
-            echo "*** Unsupported Windows build type: \"${NAME}\" used from \"${BUILD}\""
-            exit 1
-          ;;
-        esac
-
-        echo "* NAME: ${NAME}, WINDOWS_ARCH: ${WINDOWS_ARCH}, WINDOWS_VERSION: ${WINDOWS_VERSION}, WINDOWS_EDITION: ${WINDOWS_EDITION}"
-        ISO_CHECKSUM=$(awk "/$(basename ${ISO_URL})/ { print \$1 }" win_iso.sha256)
-        if [[ ${PACKER_VAGRANT_PROVIDER} = "libvirt" ]]; then
-          test -f "${VIRTIO_WIN_ISO}" || curl -sL "${VIRTIO_WIN_ISO_URL}" --output "${VIRTIO_WIN_ISO}"
-          if [[ ! -d "${VIRTIO_WIN_ISO_DIR}" ]]; then
-            xorriso -report_about SORRY -osirrox on -indev "${VIRTIO_WIN_ISO}" -extract / "${VIRTIO_WIN_ISO_DIR}"
-            find "${VIRTIO_WIN_ISO_DIR}" -type d -exec chmod u+rwx {} \;
-          fi
-        fi
-      ;;
+      case ${NAME} in
+      *windows-10-enterprise*)
+        export ISO_URL="https://software-download.microsoft.com/download/sg/444969d5-f34g-4e03-ac9d-1f9786c69161/19044.1288.211006-0501.21h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
+        ;;
+      *windows-server-2022-*)
+        export WINDOWS_TYPE="server"
+        export ISO_URL="https://software-download.microsoft.com/download/sg/20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
+        ;;
+      *windows-server-2019-*)
+        export WINDOWS_TYPE="server"
+        export ISO_URL="https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso"
+        ;;
+      *windows-server-2016-*)
+        export WINDOWS_TYPE="server"
+        export ISO_URL="https://software-download.microsoft.com/download/pr/Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO"
+        ;;
       *)
-        echo "*** Unsupported build type: \"${NAME}\" used from \"${BUILD}\""
+        echo "*** Unsupported Windows build type: \"${NAME}\" used from \"${BUILD}\""
         exit 1
+        ;;
+      esac
+
+      echo "* NAME: ${NAME}, WINDOWS_ARCH: ${WINDOWS_ARCH}, WINDOWS_VERSION: ${WINDOWS_VERSION}, WINDOWS_EDITION: ${WINDOWS_EDITION}"
+      ISO_CHECKSUM=$(awk "/$(basename ${ISO_URL})/ { print \$1 }" win_iso.sha256)
+      if [[ ${PACKER_VAGRANT_PROVIDER} = "libvirt" ]]; then
+        test -f "${VIRTIO_WIN_ISO}" || curl -sL "${VIRTIO_WIN_ISO_URL}" --output "${VIRTIO_WIN_ISO}"
+        if [[ ! -d "${VIRTIO_WIN_ISO_DIR}" ]]; then
+          xorriso -report_about SORRY -osirrox on -indev "${VIRTIO_WIN_ISO}" -extract / "${VIRTIO_WIN_ISO_DIR}"
+          find "${VIRTIO_WIN_ISO_DIR}" -type d -exec chmod u+rwx {} \;
+        fi
+      fi
+      ;;
+    *)
+      echo "*** Unsupported build type: \"${NAME}\" used from \"${BUILD}\""
+      exit 1
       ;;
     esac
 
@@ -185,18 +185,15 @@ cmdline() {
   done
 }
 
-
 packer_build() {
   if [[ ! -f "${PACKER_IMAGES_OUTPUT_DIR}/${BUILD}.box" ]]; then
     echo "*** Running packer with params: ${PACKER_CMD_PARAMS[*]}"
     ${PACKER_BINARY} "${PACKER_CMD_PARAMS[@]}" 2>&1 | tee "${LOGDIR}/${BUILD}-packer.log"
     ln -rfs "${PACKER_CACHE_DIR}/$(echo -n "${ISO_CHECKSUM}" | sha1sum | awk '{ print $1 }').iso" "${PACKER_CACHE_DIR}/${NAME}.iso"
   else
-    echo -e "\n* File ${PACKER_IMAGES_OUTPUT_DIR}/${BUILD}.box already exists. Skipping....\n";
+    echo -e "\n* File ${PACKER_IMAGES_OUTPUT_DIR}/${BUILD}.box already exists. Skipping....\n"
   fi
 }
-
-
 
 #######
 # Main
--- upload_box_to_vagrantcloud.sh
+++ upload_box_to_vagrantcloud.sh
@@ -8,7 +8,6 @@ PROGNAME=$(basename "$0")
 readonly PROGNAME
 readonly ARGS=$*
 
-
 usage() {
   cat <<- EOF
 
@@ -50,7 +49,7 @@ cmdline() {
 
   if [[ -z "${USER_BOX}" ]] || [[ -z "${VAGRANT_CLOUD_TOKEN}" ]]; then
     usage
-    exit 1;
+    exit 1
   fi
 
   export VAGRANT_CLOUD_USER="${USER_BOX%@*}"
@@ -70,85 +69,85 @@ cmdline() {
   echo "*** My Name: ${MY_NAME} | User: ${VAGRANT_CLOUD_USER}  | Provider: ${VAGRANT_PROVIDER} | Box file: ${VAGRANT_CLOUD_BOX_FILE} | Box name: ${VAGRANT_CLOUD_BOX_NAME}"
 
   case ${VAGRANT_CLOUD_BOX_NAME} in
-    *centos*)
-      CENTOS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
-      export CENTOS_VERSION
-      CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
-      export CENTOS_TAG
-      CENTOS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
-      export CENTOS_ARCH
-      export CENTOS_TYPE="NetInstall"
-      export NAME="${MY_NAME}-${CENTOS_VERSION}-${CENTOS_ARCH}"
-      export SHORT_DESCRIPTION="My CentOS ${CENTOS_VERSION} ${CENTOS_ARCH} for libvirt and virtualbox"
-      LONG_DESCRIPTION=$(envsubst < templates/my_centos.md)
-      export LONG_DESCRIPTION
+  *centos*)
+    CENTOS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
+    export CENTOS_VERSION
+    CENTOS_TAG=$(curl -s "ftp://ftp.cvut.cz/centos/${CENTOS_VERSION}/isos/x86_64/sha256sum.txt" | sed -n 's/.*-\(..\)\(..\)\.iso/\1\2/p' | head -1)
+    export CENTOS_TAG
+    CENTOS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
+    export CENTOS_ARCH
+    export CENTOS_TYPE="NetInstall"
+    export NAME="${MY_NAME}-${CENTOS_VERSION}-${CENTOS_ARCH}"
+    export SHORT_DESCRIPTION="My CentOS ${CENTOS_VERSION} ${CENTOS_ARCH} for libvirt and virtualbox"
+    LONG_DESCRIPTION=$(envsubst < templates/my_centos.md)
+    export LONG_DESCRIPTION
     ;;
-    *ubuntu*)
-      UBUNTU_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
-      export UBUNTU_TYPE
-      export UBUNTU_TYPE_UC=${UBUNTU_TYPE^}
-      UBUNTU_MAJOR_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
-      export UBUNTU_MAJOR_VERSION
-      UBUNTU_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
-      export UBUNTU_ARCH
-      UBUNTU_VERSION=$(curl -s "http://releases.ubuntu.com/${UBUNTU_MAJOR_VERSION}/SHA1SUMS" | sed -n "s/.*ubuntu-\([^-]*\)-.*-${UBUNTU_ARCH}.iso/\1/p" | head -1)
-      export UBUNTU_VERSION
-      export NAME="${MY_NAME}-${UBUNTU_MAJOR_VERSION}-${UBUNTU_TYPE}-${UBUNTU_ARCH}"
-      export SHORT_DESCRIPTION="Ubuntu ${UBUNTU_MAJOR_VERSION} ${UBUNTU_TYPE} (${UBUNTU_ARCH}) for libvirt and virtualbox"
-      LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}.md")
-      export LONG_DESCRIPTION
+  *ubuntu*)
+    UBUNTU_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
+    export UBUNTU_TYPE
+    export UBUNTU_TYPE_UC=${UBUNTU_TYPE^}
+    UBUNTU_MAJOR_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
+    export UBUNTU_MAJOR_VERSION
+    UBUNTU_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
+    export UBUNTU_ARCH
+    UBUNTU_VERSION=$(curl -s "http://releases.ubuntu.com/${UBUNTU_MAJOR_VERSION}/SHA1SUMS" | sed -n "s/.*ubuntu-\([^-]*\)-.*-${UBUNTU_ARCH}.iso/\1/p" | head -1)
+    export UBUNTU_VERSION
+    export NAME="${MY_NAME}-${UBUNTU_MAJOR_VERSION}-${UBUNTU_TYPE}-${UBUNTU_ARCH}"
+    export SHORT_DESCRIPTION="Ubuntu ${UBUNTU_MAJOR_VERSION} ${UBUNTU_TYPE} (${UBUNTU_ARCH}) for libvirt and virtualbox"
+    LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}.md")
+    export LONG_DESCRIPTION
     ;;
-    *windows-10*)
-      WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
-      export WINDOWS_VERSION
-      WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
-      export WINDOWS_ARCH
-      WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
-      export WINDOWS_EDITION
-      export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
-      export NAME="${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-      export SHORT_DESCRIPTION="Windows ${WINDOWS_VERSION} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
-      LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-eval.md")
-      export LONG_DESCRIPTION
+  *windows-10*)
+    WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
+    export WINDOWS_VERSION
+    WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
+    export WINDOWS_ARCH
+    WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
+    export WINDOWS_EDITION
+    export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
+    export NAME="${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
+    export SHORT_DESCRIPTION="Windows ${WINDOWS_VERSION} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
+    LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-eval.md")
+    export LONG_DESCRIPTION
     ;;
-    *windows-*-2012*)
-      WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '[-_]' '{ print $3 }')
-      export WINDOWS_VERSION
-      WINDOWS_RELEASE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '[-_]' '{ print $4 }')
-      export WINDOWS_RELEASE
-      export WINDOWS_RELEASE_UC=${WINDOWS_RELEASE^}
-      WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $5 }')
-      export WINDOWS_ARCH
-      WINDOWS_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
-      export WINDOWS_TYPE
-      export WINDOWS_TYPE_UC=${WINDOWS_TYPE^}
-      WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
-      export WINDOWS_EDITION
-      export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
-      export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}_${WINDOWS_RELEASE}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-      export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} ${WINDOWS_VERSION} ${WINDOWS_RELEASE_UC} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
-      LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.md")
-      export LONG_DESCRIPTION
+  *windows-*-2012*)
+    WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '[-_]' '{ print $3 }')
+    export WINDOWS_VERSION
+    WINDOWS_RELEASE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '[-_]' '{ print $4 }')
+    export WINDOWS_RELEASE
+    export WINDOWS_RELEASE_UC=${WINDOWS_RELEASE^}
+    WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $5 }')
+    export WINDOWS_ARCH
+    WINDOWS_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
+    export WINDOWS_TYPE
+    export WINDOWS_TYPE_UC=${WINDOWS_TYPE^}
+    WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
+    export WINDOWS_EDITION
+    export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
+    export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}_${WINDOWS_RELEASE}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
+    export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} ${WINDOWS_VERSION} ${WINDOWS_RELEASE_UC} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
+    LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.md")
+    export LONG_DESCRIPTION
     ;;
-    *windows-*-201[69]*|*windows-*-2022*)
-      WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
-      export WINDOWS_VERSION
-      WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $5 }')
-      export WINDOWS_ARCH
-      WINDOWS_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
-      export WINDOWS_TYPE
-      export WINDOWS_TYPE_UC=${WINDOWS_TYPE^}
-      WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
-      export WINDOWS_EDITION
-      export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
-      export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
-      export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} ${WINDOWS_VERSION} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
-      LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.md")
-      export LONG_DESCRIPTION
+  *windows-*-201[69]* | *windows-*-2022*)
+    WINDOWS_VERSION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $3 }')
+    export WINDOWS_VERSION
+    WINDOWS_ARCH=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $5 }')
+    export WINDOWS_ARCH
+    WINDOWS_TYPE=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $2 }')
+    export WINDOWS_TYPE
+    export WINDOWS_TYPE_UC=${WINDOWS_TYPE^}
+    WINDOWS_EDITION=$(echo "${VAGRANT_CLOUD_BOX_NAME}" | awk -F '-' '{ print $4 }')
+    export WINDOWS_EDITION
+    export WINDOWS_EDITION_UC=${WINDOWS_EDITION^}
+    export NAME="${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-${WINDOWS_EDITION}-${WINDOWS_ARCH}-eval"
+    export SHORT_DESCRIPTION="Windows ${WINDOWS_TYPE_UC} ${WINDOWS_VERSION} ${WINDOWS_EDITION_UC} (${WINDOWS_ARCH}) Evaluation for libvirt and virtualbox"
+    LONG_DESCRIPTION=$(envsubst < "templates/${MY_NAME}-${WINDOWS_TYPE}-${WINDOWS_VERSION}-eval.md")
+    export LONG_DESCRIPTION
     ;;
-    *)
-      echo "*** Unsupported type: \"${VAGRANT_CLOUD_BOX_NAME}\""
-      exit 1
+  *)
+    echo "*** Unsupported type: \"${VAGRANT_CLOUD_BOX_NAME}\""
+    exit 1
     ;;
   esac
 
@@ -171,7 +170,7 @@ cmdline() {
 
   # Check if the version already exists otherwise create new one
   BOX_CREATED_AT=$(curl -s "https://vagrantcloud.com/api/v1/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}" | jq -r '.created_at')
-  if [[ "${BOX_CREATED_AT}" == "null" ]] ; then
+  if [[ "${BOX_CREATED_AT}" == "null" ]]; then
     echo "*** Create new version: ${VAGRANT_CLOUD_USER}/${NAME} | ${BOX_VERSION}"
     vagrant cloud version create --description "${LONG_DESCRIPTION}" "${VAGRANT_CLOUD_USER}/${NAME}" "${BOX_VERSION}"
   else
@@ -179,7 +178,7 @@ cmdline() {
     # Check if the box version = ${BOX_VERSION} with different GIT_SHA commit already exists then delete the box version
     # This may happen if you try to upload box images multiple times per day
     # GITHUB_SHA is defined during GitGub Action execution
-    if ! curl -s "https://vagrantcloud.com/api/v1/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/" | jq '.description_markdown' | grep -q "${GITHUB_SHA}" ; then
+    if ! curl -s "https://vagrantcloud.com/api/v1/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/" | jq '.description_markdown' | grep -q "${GITHUB_SHA}"; then
       echo "*** Delete the box version \"${BOX_VERSION}\" with old git sha"
       vagrant cloud version delete -f "${VAGRANT_CLOUD_USER}/${NAME}" "${BOX_VERSION}"
       echo "*** Create new version: ${VAGRANT_CLOUD_USER}/${NAME} | ${BOX_VERSION}"
@@ -188,7 +187,7 @@ cmdline() {
   fi
 
   # Check if you are not uploading the box with the checksum which is already there
-  if [[ $(curl -s "https://vagrantcloud.com/api/v1/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/provider/${VAGRANT_PROVIDER}" | jq -r '.checksum') != "${CHECKSUM_BOX_FILE}" ]] ; then
+  if [[ $(curl -s "https://vagrantcloud.com/api/v1/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/provider/${VAGRANT_PROVIDER}" | jq -r '.checksum') != "${CHECKSUM_BOX_FILE}" ]]; then
     echo "*** Create new provider: ${VAGRANT_CLOUD_USER}/${NAME} | ${BOX_VERSION} | ${VAGRANT_PROVIDER} | ${CHECKSUM_BOX_FILE}"
     vagrant cloud provider create --checksum-type sha256 --checksum "${CHECKSUM_BOX_FILE}" "${VAGRANT_CLOUD_USER}/${NAME}" "${VAGRANT_PROVIDER}" "${BOX_VERSION}"
     VAGRANTCLOUD_UPLOAD_PATH=$(curl -sL "https://vagrantcloud.com/api/v1/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/provider/${VAGRANT_PROVIDER}/upload?access_token=$VAGRANT_CLOUD_TOKEN" | jq -r '.upload_path')
@@ -199,7 +198,6 @@ cmdline() {
     echo "*** Skipping upload..."
   fi
 }
-
 
 #######
 # Main
